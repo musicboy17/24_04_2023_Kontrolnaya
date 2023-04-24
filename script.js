db469899c1bd784f0426cea479b8493b37684b5e
musicboy17 <pratham@yandex.ru>
2023-04-24 06:23:11 +0300

    Forth step

---
+++ script.js
@@ -0,0 +1,16 @@
+const arr = [];
+let i = 0;
+let j = 0;
+str1 = prompt('Введите 1-ый элемент массива:');
+str2 = prompt('Введите 2-ой элемент массива::');
+str3 = prompt('Введите 3-ий элемент массива::');
+
+
+const arr1 = [str1, str2, str3];
+
+//alert(arr1);
+
+for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i].length <= 2) { arr[j] = arr1[i] } else i++;
+}
+alert(arr);
